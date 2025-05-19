8ff4f07cd44e84d873eda08b6e3aa02151085b25
Kalicz Péter <kaliczp@gmail.com>
2025-05-19 11:36:47 +0200

    Advanced

--- PCA.R
+++ PCA.R
@@ -4,3 +4,15 @@ ktt <- ktt[,-ncol(ktt)] # Utolsó oszlop (összeg) törölve
 ?prcomp # Help
 ktt1.pr <- prcomp(ktt[,11:ncol(ktt)]) # PCA OC data
 summary(ktt1.pr) # PCs
+
+ktt1.pr <- prcomp(ktt[,11:ncol(ktt)], rank = 2) # PCA OC data
+summary(ktt1.pr) # PCs
+plot(ktt1.pr) # components
+biplot(ktt1.pr) # components
+
+## Elegyaránnyal
+kttea <- ktt[,c(8,11:ncol(ktt))]
+kttea$Elegyarány <- as.numeric(kttea$Elegyarány)
+ktt2.pr <- prcomp(kttea)
+plot(ktt2.pr)
+biplot(ktt2.pr, scale = 0)
