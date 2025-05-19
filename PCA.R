7241fc200073d6de09a42cd7e94bb35ab24dc17e
Kalicz Péter <kaliczp@gmail.com>
2025-05-19 11:10:13 +0200

    First PCA

--- PCA.R
+++ PCA.R
@@ -1,3 +1,6 @@
 library(readxl)
 ktt <- as.data.frame(read_excel("klaszter.xlsx")) #Beolvasás
 ktt <- ktt[,-ncol(ktt)] # Utolsó oszlop (összeg) törölve
+?prcomp # Help
+ktt1.pr <- prcomp(ktt[,11:ncol(ktt)]) # PCA OC data
+summary(ktt1.pr) # PCs
